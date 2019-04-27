cd2b0d8a21a7c0abd754cdb79d993bdd964d4642
Norris Wise Jr <wise.norris1@gmail.com>
2019-04-26 20:48:54 -0400

    Initial Commit

---
+++ ShowtimesApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShowtimesApp
+//
+//  Created by Norris Wise Jr on 4/26/19.
+//  Copyright © 2019 Norris Wise Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
